e57020afae15ee164f1c4282fe701e8fefb80a1e
shinyaigeek <philispaxil@gmail.com>
2021-08-17 16:32:05 +0900

    Feature: init vertual machine

--- src/bytecode/bytecode/opcode.rs
+++ src/bytecode/bytecode/opcode.rs
@@ -1,5 +1,8 @@
-pub enum OpCode {
-    Increment,
-    Decrement,
-    Done,
+pub enum Opcode {
+    Load(usize, i64),
+    Add(usize, usize, usize),
+    Sub(usize, usize, usize),
+    Div(usize, usize, usize),
+    Mul(usize, usize, usize),
+    Done(usize),
 }
--- src/bytecode/interpreter/vm.rs
+++ src/bytecode/interpreter/vm.rs
@@ -1,1 +1,23 @@
+use crate::bytecode::bytecode::opcode::OpCode;
 
+const REGISTER_LENGTH: usize = 16;
+
+pub struct VM {
+    ip: u8,
+    accumulator: u64,
+    pc: u8,
+    program: Vec<OpCode>,
+    register: [i64; REGISTER_LENGTH],
+}
+
+impl VM {
+    pub fn init() -> Self {
+        Self {
+            ip: 0,
+            accumulator: 0,
+            pc: 0,
+            program: Vec::new(),
+            register: [0; REGISTER_LENGTH],
+        }
+    }
+}
